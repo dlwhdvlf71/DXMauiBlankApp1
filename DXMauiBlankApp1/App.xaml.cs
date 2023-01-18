61024997a526f2a25705b3f69a307c5f1dcf36a4
Anony <41979552+dlwhdvlf71@users.noreply.github.com>
2023-01-18 09:45:33 +0900

    초기 커밋.

---
+++ DXMauiBlankApp1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DXMauiBlankApp1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ DXMauiBlankApp1/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace DXMauiBlankApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public bool OnLeave { get; set; }
+    }
+}
